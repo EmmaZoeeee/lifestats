9d0afd686f3fdf041c99497e42c55e2a3aae5050
EmmaZoeeee <emma.wallinrosell@elev.ga.ntig.se>
2025-03-28 10:37:44 +0100

    ...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,138 @@
-<main class="grid grid-cols-[30%_40%_30%] grid-rows-[10%_10%_10%_5%_5%_5%_45%]">
+
+<script>
+    // @ts-nocheck
+    
+        import { onMount } from "svelte";
+      
+        // Fält för att mata in födelsedatum
+        let birthDay = 0;
+        let birthMonth = 0;
+        let birthYear = 0;
+      
+        /*
+         @type {string | any[]}
+         */
+        let results = [];
+        let errorMessage = "";
+    
+        onMount(() => {
+            console.log('the component has mounted');
+        });
+      
+        // Lista med beräkningar
+        const calculations = [
+          { name: "Dagar levt", formula: (/** @type {number} */ ms) => Math.floor(ms / (1000 * 60 * 60 * 24)) },
+          { name: "Timmar levt", formula: (/** @type {number} */ ms) => Math.floor(ms / (1000 * 60 * 60)) },
+          { name: "Minuter levt", formula: (/** @type {number} */ ms) => Math.floor(ms / (1000 * 60)) },
+          { name: "Sekunder levt", formula: (/** @type {number} */ ms) => Math.floor(ms / 1000) }
+        ];
+      
+        function calculate() {
+            console.log("Calculating!")
+          errorMessage = "";
+          results = [];
+      
+          // Kontrollera att alla fält är ifyllda
+          if (birthDay <= 0 || birthMonth <= 0 || birthYear <= 0) {
+         errorMessage = "Fill in all fields correctly!";
+         return;
+        }
+      
+          // Skapa ett datumobjekt från inmatningen
+          let birthDate = new Date(birthYear, birthMonth - 1, birthDay);
+      
+          // Kontrollera att datumet är giltigt
+          if (isNaN(birthDate.getTime())) {
+            errorMessage = "Invalid date, please check your input.";
+            return;
+          }
+      
+          let now = new Date();
+          // @ts-ignore
+          let timeLived = now - birthDate;
+      
+          // Kontrollera att födelsedatumet inte är i framtiden
+          if (timeLived < 0) {
+            errorMessage = "The date cannot be in the future!";
+            return;
+          }
+      
+          // Beräkna värden baserat på tid i millisekunder
+          results = calculations.map(calc => ({
+            name: calc.name,
+            value: calc.formula(timeLived).toLocaleString() // Formaterar siffror snyggt
+          }));
+        }
+    
+      </script>
+
+
+
+
+<main class="grid grid-cols-[30%_40%_30%] grid-rows-[10%_10%_10%_5%_5%_10%_40%]">
 
     <div class="rubrik col-start-2 row-start-2 justify-self-center"><h1>LIFESTATS</h1></div>
-    <div class="col-start-2 row-start-4 justify-self-center"> Your birthdate:</div>
-    <div class="col-start-2 row-start-5"> Month</div>
-    <div class="col-start-2 row-start-5 justify-self-center"> Day </div>
-    <div class="col-start-2 row-start-5 justify-self-end"> Year</div>
-    <div class="col-start-2 row-start-6 justify-self-center"> Calculate</div>
-    <div id="bild" class="col-start-2 row-start-7"><img src="/earth.png" alt="earth"></div>
-        
- 
+    
+    <div id="type" class="col-start-2 row-start-4 justify-self-center"> Your birthdate:</div>
+
+    <div id="day" class="col-start-2 row-start-5 justify-self-start m-3">         
+        <input class="bibi w-20" type="number" placeholder="Day" min="1" max="31"
+        on:input={(e) => birthDay = +e.target.value}>   
+    </div>
+
+    <div id="month" class="col-start-2 row-start-5 justify-self-center m-3">         
+        <input class="bibi w-20" type="number" placeholder="Month" min="1" max="12"
+        on:input={(e) => birthMonth = +e.target.value}>    
+    </div>
+
+    <div id="year" class="col-start-2 row-start-5 justify-self-end m-3">         
+        <input class="bibi w-20" type="number" placeholder="Year" min="1900" max="2025"
+        on:input={(e) => birthYear = +e.target.value}>    
+    </div>
+   
+    <div id="calculate" class="col-start-2 row-start-6 justify-self-center m-8">
+        <button on:click={() => { console.log("Klickade på knappen!"); calculate(); }}>
+            Calculate
+          </button>
+          
+    </div>
+
+    <div id="bild" class="col-start-2 row-start-7">
+        <img src="/earth.png" alt="earth">
+    </div>
+    
+    <div class="fixed bottom-30 justify-self-center">
+       {#if errorMessage}
+            <p class="error">{errorMessage}</p>
+        {/if}
+    
+        {#if results.length > 0}
+            <ul>
+                {#each results as result}
+                    <li>{result.name}: <strong>{result.value}</strong></li>
+                 {/each}
+            </ul>
+        {/if}
+    </div>
+    
+
 </main>
+
+
+
+
+
+
 
 
 <style>
 
-@font-face {
+ @font-face {
     font-family: 'champagne';
     /* font-style: normal; */
     /* font-weight: 500; */
-    src: url('../../static/Champagne.ttf') format('truetype');
-}
+    /* src: url('../../static/Champagne.ttf') format('truetype'); */
+ }
     main {
         background-color: rgb(69, 84, 114);
     }
@@ -27,6 +140,11 @@
     #bild {
         /* display: flex;
         justify-content: center; */
+        opacity: 0;
+    }
+
+    #result {
+        height: 100%;
     }
 
     h1 {
@@ -39,4 +157,68 @@
         /* padding: 50px; */
     }
 
+    button {
+      padding: 0.7rem 1.5rem;
+      background-color: rgb(43, 56, 82);
+      font-size: 20pt;
+      color: white;
+      border: none; 
+      border-radius: 0px;
+      cursor: pointer;
+
+    }
+    button:hover {
+      background-color: rgb(95, 112, 145);
+    }
+
+    .error {
+      color: rgb(179, 126, 126);
+      font-weight: bold;
+      margin-top: 1rem;
+    }
+
+    #day input {
+        background-color: rgb(95, 112, 145);
+        color: #ffffff;
+        border: none;
+    }
+
+    #month input {
+        background-color: rgb(95, 112, 145);
+        color: #ffffff;
+        border: none;
+    }
+
+    #year input {
+        background-color: rgb(95, 112, 145);
+        color: #ffffff;
+        border: none;
+    }
+
+    .bibi::placeholder {
+        color: #ffffff;
+    }
+
+    #type {
+        color:#ffffff;
+        font-size: 20pt;
+    }
+
 </style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

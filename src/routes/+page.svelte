5a150b7a0b8e16e60f73cafe445b27f805e9cf64
EmmaZoeeee <emma.wallinrosell@elev.ga.ntig.se>
2025-04-26 20:39:14 +0200

    ...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,120 +1,40 @@
 
 <script>
-    // @ts-nocheck
+
+    let days = $state(0)
+
+    let weeks = $derived(days / 7)
+
+    let year = $derived(days / 365)
+
+    let andetag = $derived(days * 25000)
+
+</script>
+
+<main class="grid grid-cols-[30%_40%_30%] grid-rows-[10%_10%_10%_10%_5%_5%_10%_30%_30%_30%_30%]">
+
+    <div id="rubrik" class="rubrik col-start-2 row-start-2 justify-self-center"><h1>LIFESTATS</h1></div>
     
-        import { onMount } from "svelte";
-      
-        // Fält för att mata in födelsedatum
-        let birthDay = 0;
-        let birthMonth = 0;
-        let birthYear = 0;
-      
-        /*
-         @type {string | any[]}
-         */
-        let results = [];
-        let errorMessage = "";
-    
-        onMount(() => {
-            console.log('the component has mounted');
-        });
-      
-        // Lista med beräkningar
-        const calculations = [
-          { name: "Dagar levt", formula: (/** @type {number} */ ms) => Math.floor(ms / (1000 * 60 * 60 * 24)) },
-          { name: "Timmar levt", formula: (/** @type {number} */ ms) => Math.floor(ms / (1000 * 60 * 60)) },
-          { name: "Minuter levt", formula: (/** @type {number} */ ms) => Math.floor(ms / (1000 * 60)) },
-          { name: "Sekunder levt", formula: (/** @type {number} */ ms) => Math.floor(ms / 1000) }
-        ];
-      
-        function calculate() {
-            console.log("Calculating!")
-          errorMessage = "";
-          results = [];
-      
-          // Kontrollera att alla fält är ifyllda
-          if (birthDay <= 0 || birthMonth <= 0 || birthYear <= 0) {
-         errorMessage = "Fill in all fields correctly!";
-         return;
-        }
-      
-          // Skapa ett datumobjekt från inmatningen
-          let birthDate = new Date(birthYear, birthMonth - 1, birthDay);
-      
-          // Kontrollera att datumet är giltigt
-          if (isNaN(birthDate.getTime())) {
-            errorMessage = "Invalid date, please check your input.";
-            return;
-          }
-      
-          let now = new Date();
-          // @ts-ignore
-          let timeLived = now - birthDate;
-      
-          // Kontrollera att födelsedatumet inte är i framtiden
-          if (timeLived < 0) {
-            errorMessage = "The date cannot be in the future!";
-            return;
-          }
-      
-          // Beräkna värden baserat på tid i millisekunder
-          results = calculations.map(calc => ({
-            name: calc.name,
-            value: calc.formula(timeLived).toLocaleString() // Formaterar siffror snyggt
-          }));
-        }
-
-    
-      </script>
-
-
-
-
-<main class="grid grid-cols-[30%_40%_30%] grid-rows-[10%_10%_10%_5%_5%_10%_40%]">
-
-    <div class="rubrik col-start-2 row-start-2 justify-self-center"><h1>LIFESTATS</h1></div>
-    
-    <div id="type" class="col-start-2 row-start-4 row-end-7 justify-self-center w-139 h-12"> Your birthdate:</div>
-
-    <div id="day" class="col-start-2 row-start-5 justify-self-start m-3">         
-        <input class="bibi w-25" type="number" placeholder="Day" min="1" max="31"
-        on:input={(e) => birthDay = +e.target.value}>   
-    </div>
-
-    <div id="month" class="col-start-2 row-start-5 justify-self-center m-3">         
-        <input class="bibi w-25" type="number" placeholder="Month" min="1" max="12"
-        on:input={(e) => birthMonth = +e.target.value}>    
-    </div>
-
-    <div id="year" class="col-start-2 row-start-5 justify-self-end m-3">         
-        <input class="bibi w-25" type="number" placeholder="Year" min="1900" max="2025"
-        on:input={(e) => birthYear = +e.target.value}>    
-    </div>
+    <div id="type" class="col-start-2 row-start-5 row-end-5"> Hur många dagar är du? </div>
    
-    <div id="calculate" class="col-start-2 row-start-6 justify-self-center m-15">
-        <button class="button" on:click={() => { console.log("Klickade på knappen!"); calculate(); }}>
-            Calculate
-          </button>
-          
-    </div>
+    <input id="skriv" class="col-start-2 row-start-6 row-end-6 h-[50px]" type="number" bind:value={days} />
 
-    <div id="bild" class="col-start-2 row-start-7">
-        <img src="/earth.png" alt="earth">
-    </div>
-    
-    <div class="fixed bottom-30 justify-self-center">
-       {#if errorMessage}
-            <p class="error">{errorMessage}</p>
-        {/if}
-    
-        {#if results.length > 0}
-            <ul class="result">
-                {#each results as result}
-                    <li>{result.name}: <strong>{result.value}</strong></li>
-                 {/each}
-            </ul>
-        {/if}
-    </div>
+    <section id="dagarlevt" class="col-start-1 col-end-4 row-start-8 row-end-8">
+        <p>Du har levt {days} dagar</p>
+    </section>
+
+    <section id="veckorlevt" class="col-start-1 col-end-4 row-start-9 row-end-9">
+        <p>Du har levt {weeks} veckor</p>
+    </section>
+
+    <section id="årlevt" class="col-start-1 col-end-4 row-start-10 row-end-10">
+        <p>Du har levt {year} år</p>
+    </section>
+
+    <section id="årlevt" class="col-start-1 col-end-4 row-start-11 row-end-11">
+        <p>Du har tagit {andetag} andetag</p>
+    </section>
+
 </main>
 
 
@@ -128,17 +48,44 @@
  }
     main {
         /* background-color: rgb(121, 136, 164); */
-        background-color: rgb(6, 10, 17);
+        background-color: rgb(18, 28, 45);
+        /* background-color: rgb(125, 153, 201); */
+        height: 100vh;
+    }
+
+    #dagarlevt {
+        /* background-color: #ffffff; */
+        color:#ffffff;
+        border: none;
+        display: flex;
+        justify-content: center;
+    }
+
+    #veckorlevt {
+        background-color:rgb(18, 28, 45);
+        color:#ffffff;
+        border: none;
+        display: flex;
+        justify-content: center;
+    }
+
+    #årlevt {
+        color:#ffffff;
+        display: flex;
+        justify-content: center;
+        background-color: rgb(18, 28, 45);
+    }
+
+    #skriv {
+        border: none;
+        border: none;
+        border: solid 1px rgb(18, 28, 45);
     }
 
     #bild {
         /* display: flex;
         justify-content: center; */
         opacity: 0;
-    }
-
-    #result {
-        height: 100%;
     }
 
     h1 {
@@ -194,13 +141,15 @@
     }
 
     #type {
-        color:#ffffff;
-        background-color: rgb(69, 84, 114);
-        font-size: 20pt;
+        background-color:#ffffff;
+        color: black;
+        /* color: rgb(69, 84, 114); */
+        /* font-size: 20pt; */
         /* border: 2px solid rgb(69, 84, 114); */
+        border: solid 1px rgb(18, 28, 45);
         display: flex;
-        justify-content: center;
         align-items: center;
+        padding-left: 10px;
     }
 
     .button {
@@ -209,6 +158,10 @@
 
     .result {
         color: white;
+    }
+
+    #rubrik {
+        font-family:Verdana, Geneva, Tahoma, sans-serif;
     }
 
 </style>
